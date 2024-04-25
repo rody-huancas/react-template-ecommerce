8af0514fa7630ce3a3ddb3acdb0dfe077baaa71c
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-24 20:02:24 -0500

    Update: menu responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,28 @@
+import classNames from "classnames";
+import { useEffect, useState } from "react";
 import { RiEditLine, RiHeart3Line, RiLockPasswordLine, RiSearchLine, RiShoppingCartLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import "./header.css"
 
 export const Header = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header className="w-full font-poppins text-tertiary/80">
-      <div className="w-6xl mx-auto flex justify-between items-center text-xs uppercase font-semibold py-2">
+      <div className="lg:w-6xl px-10 lg:px-0 mx-auto flex justify-between items-center text-xs uppercase font-semibold py-2">
         <Link to={"/"} className="flex items-center gap-2">
           <span>Registrarse</span>
           <RiEditLine />
@@ -17,25 +35,29 @@ export const Header = () => {
 
       <hr className="border border-gray-900/30" />
 
-      <nav className="w-6xl mx-auto flex items-center justify-between py-2 text-tertiary/70">
-        <div className="font-semibold uppercase flex items-center gap-3">
-          <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Hombre</Link>
-          <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Mujer</Link>
-          <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Niños</Link>
-        </div>
+      <nav className={classNames("md:w-full md:mx-auto md:flex md:justify-center md:bg-white nav__responsive", {
+        "nav__scrolled": scrollPosition > 30
+      })}>
+        <div className="text-tertiary/70 items__menu">
+          <Link to="/" className="text-4xl uppercase font-extrabold text-tertiary/90 font-ace">Logo</Link>
 
-        <Link to="/" className="text-4xl uppercase font-extrabold text-tertiary/90 font-ace">Logo</Link>
-
-        <div className="flex items-center gap-4">
-          <button>
-            <RiHeart3Line className="hover:text-tertiary transition-colors duration-300" />
-          </button>
-          <button>
-            <RiShoppingCartLine className="hover:text-tertiary transition-colors duration-300" />
-          </button>
-          <button>
-            <RiSearchLine className="hover:text-tertiary transition-colors duration-300" />
-          </button>
+          <div className="items">
+            <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Hombre</Link>
+            <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Mujer</Link>
+            <Link to={"/"} className="text-sm hover:text-tertiary transition-colors duration-300">Niños</Link>
+          </div>
+                  
+          <div className="flex items-center gap-4">
+            <button>
+              <RiHeart3Line className="hover:text-tertiary transition-colors duration-300" />
+            </button>
+            <button>
+              <RiShoppingCartLine className="hover:text-tertiary transition-colors duration-300" />
+            </button>
+            <button>
+              <RiSearchLine className="hover:text-tertiary transition-colors duration-300" />
+            </button>
+          </div>
         </div>
       </nav>
     </header>
