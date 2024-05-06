ccebef422a7a2eda852e23c41e06ef9e98bf8997
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-06 18:43:22 -0500

    Fix: repsonsive in detil product

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -6,14 +6,14 @@ const ItemDetail = () => {
   const [selectedImage, setSelectedImage] = useState("/cards/1.png");
 
   return (
-    <section className="w-6xl mx-auto my-10">
-      <h1 className="text-5xl font-extrabold text-tertiary/90 uppercase font-poppins">
+    <section className="w-full flex flex-col xl:w-6xl mx-auto my-10 px-5">
+      <h1 className="text-2xl md:text-5xl font-extrabold text-tertiary/90 uppercase font-poppins">
         Detalles de Producto
       </h1>
-      <div className="w-full flex items-start justify-center gap-16 mt-10">
-        <div className="w-1/2">
+      <div className="w-full flex flex-col lg:flex-row items-center lg:items-start justify-center gap-16 mt-10 ">
+        <div className="w-full md:w-1/2">
           {selectedImage && (
-            <div className="w-full h-96 shadow-lg flex items-center justify-center rounded-xl">
+            <div className="w-full h-80 shadow-lg flex items-center justify-center rounded-xl">
               <img src={selectedImage} alt="Producto" className="object-cover" />
             </div>
           )}
@@ -22,19 +22,19 @@ const ItemDetail = () => {
             <img
               src="/cards/1.png"
               alt="Producto"
-              className="p-2 bg-gray-200 h-32 w-full object-cover cursor-pointer rounded-xl"
+              className="p-2 bg-gray-200 h-24 sm:h-32 w-full object-cover cursor-pointer rounded-xl"
               onClick={() => setSelectedImage("/cards/1.png")}
             />
             <img
               src="/cards/2.png"
               alt="Producto"
-              className="p-2 bg-gray-200 h-32 w-full object-cover cursor-pointer rounded-xl"
+              className="p-2 bg-gray-200 h-24 sm:h-32 w-full object-cover cursor-pointer rounded-xl"
               onClick={() => setSelectedImage("/cards/2.png")}
             />
             <img
               src="/cards/3.png"
               alt="Producto"
-              className="p-2 bg-gray-200/50 h-32 w-full object-cover cursor-pointer rounded-xl"
+              className="p-2 bg-gray-200/50 h-24 sm:h-32 w-full object-cover cursor-pointer rounded-xl"
               onClick={() => setSelectedImage("/cards/3.png")}
             />
           </div>
